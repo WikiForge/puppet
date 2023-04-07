d4b62225d2849769869bfed650c13ec7772aff84
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-04-06 22:21:11 -0600

    Try using branch directly again

--- modules/mediawiki/manifests/extensionsetup.pp
+++ modules/mediawiki/manifests/extensionsetup.pp
@@ -18,16 +18,14 @@ class mediawiki::extensionsetup {
     $repos = loadyaml("${module_path}/data/mediawiki-repos.yaml")
 
     $repos.each |$name, $params| {
-        $branch = $params['branch'] ? {
-            '_branch_' => lookup('mediawiki::branch'),
-            default    => $params['branch'],
-        }
-
         git::clone { "MediaWiki ${name}":
             ensure             => present,
             directory          => "${mwpath}/${params['path']}",
             origin             => $params['repo_url'],
-            branch             => inline_template('<%= @branch.to_s %>'),
+            branch             => $params['branch'] ? {
+                '_branch_' => lookup('mediawiki::branch'),
+                default    => $params['branch'],
+            },
             owner              => 'www-data',
             group              => 'www-data',
             mode               => '0755',
