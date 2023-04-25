a25dbb00b86a8e0446dcafdd0ff38f89114f723e
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-04-25 14:52:11 -0600

    phorge: include mariadb-client

--- modules/phorge/manifests/init.pp
+++ modules/phorge/manifests/init.pp
@@ -2,7 +2,7 @@
 class phorge (
     Integer $request_timeout = lookup('phorge::php::request_timeout', {'default_value' => 60}),
 ) {
-    ensure_packages(['python3-pygments', 'subversion'])
+    ensure_packages(['mariadb-client', 'python3-pygments', 'subversion'])
 
     $fpm_config = {
         'include_path'                    => '".:/usr/share/php"',
