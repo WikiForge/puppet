cfcb2d125879e0fee18e67533bac5e189c78cfe3
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-05-05 15:51:04 -0600

    Fix

--- modules/mediawiki/files/bin/test_mw_deploy.py
+++ modules/mediawiki/files/bin/test_mw_deploy.py
@@ -250,7 +250,7 @@ def test_LangAction():
 
 
 def test_VersionsAction():
-    with patch('os.popen') as mock_popen,
+    with patch('os.popen') as mock_popen, \
          patch('os.path.exists') as mock_exists:
         mock_popen.return_value.read.return_value = '{"1.35": "1.35", "1.36": "1.36"}'
         mock_exists.return_value = True
