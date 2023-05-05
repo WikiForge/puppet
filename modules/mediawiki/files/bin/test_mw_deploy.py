473545671065c836d863375372189941022724b8
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-05-05 14:37:20 -0600

    revert and try again

--- modules/mediawiki/files/bin/test_mw_deploy.py
+++ modules/mediawiki/files/bin/test_mw_deploy.py
@@ -1,4 +1,3 @@
-import os
 import argparse
 import pytest
 from unittest.mock import MagicMock, patch
@@ -20,9 +19,9 @@ def test_get_valid_extensions():
 
     with patch('os.scandir') as mock_scandir:
         mock_cm1 = MagicMock()
-        mock_cm1.__enter__.return_value = [patch.object(MagicMock, 'name', name=name, spec_set=os.DirEntry, is_dir=lambda: True)() for name in extensions1]
+        mock_cm1.__enter__.return_value = [MagicMock(is_dir=lambda: True, name=name) for name in extensions1]
         mock_cm2 = MagicMock()
-        mock_cm2.__enter__.return_value = [patch.object(MagicMock, 'name', name=name, spec_set=os.DirEntry, is_dir=lambda: True)() for name in extensions2]
+        mock_cm2.__enter__.return_value = [MagicMock(is_dir=lambda: True, name=name) for name in extensions2]
         mock_scandir.side_effect = [mock_cm1, mock_cm2]
 
         extensions = deploy_mediawiki.get_valid_extensions(versions)
