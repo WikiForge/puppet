f99d38008e8aecc51f17ccf324c67fb1f97e49ff
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-05-09 15:29:17 -0600

    Try adding some tests

---
+++ modules/mediawiki/files/bin/test_iaupload.py
@@ -0,0 +1,50 @@
+import unittest
+import tempfile
+import os
+from datetime import datetime, timedelta
+from unittest.mock import MagicMock, patch
+from iaupload import ArchiveUploader
+
+
+class TestArchiveUploader(unittest.TestCase):
+    def setUp(self):
+        self.uploader = ArchiveUploader()
+
+    def test_args(self):
+        args = self.uploader.parser.parse_args(['--title', 'test_title', '--file', '/path/to/test_file'])
+        self.assertEqual(args.title, 'test_title')
+        self.assertEqual(args.file, '/path/to/test_file')
+        self.assertEqual(args.collection, 'opensource')
+        self.assertEqual(args.description, '')
+        self.assertEqual(args.mediatype, 'web')
+        self.assertEqual(args.subject, 'wikiforge;wikiteam')
+
+    @patch('internetarchive.get_item')
+    def test_upload(self, mock_get_item):
+        with tempfile.NamedTemporaryFile(delete=False) as f:
+            f.write(b'test data')
+
+        mock_item = MagicMock()
+        mock_get_item.return_value = mock_item
+
+        now = datetime.now()
+        mtime = (now - timedelta(days=1)).timestamp()
+        os.utime(f.name, (mtime, mtime))
+
+        self.uploader.parser.parse_args(['--title', 'test_title', '--file', f.name])
+        self.uploader.upload()
+
+        mock_item.upload.assert_called_once_with(f.name, metadata={
+            'collection': 'opensource',
+            'date': now.strftime('%Y-%m-%d'),
+            'description': '',
+            'mediatype': 'web',
+            'subject': 'wikiforge;wikiteam',
+            'title': 'test_title'
+        }, verbose=True, queue_derive=False)
+
+        os.remove(f.name)
+
+
+if __name__ == '__main__':
+    unittest.main()
