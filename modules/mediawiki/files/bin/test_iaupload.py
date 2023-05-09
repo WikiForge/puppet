004b96003a4ba739c34d4ff055d18ac269cbc449
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-05-09 15:48:09 -0600

    Try again

--- modules/mediawiki/files/bin/test_iaupload.py
+++ modules/mediawiki/files/bin/test_iaupload.py
@@ -1,6 +1,6 @@
-import argparse
 import unittest
 import tempfile
+import sys
 import os
 from datetime import datetime, timedelta
 from unittest.mock import MagicMock, patch
@@ -10,26 +10,6 @@ from iaupload import ArchiveUploader
 class TestArchiveUploader(unittest.TestCase):
     def setUp(self):
         self.uploader = ArchiveUploader()
-        self.uploader.parser = argparse.ArgumentParser(
-            description='Uploads a file to archive.org.')
-        self.uploader.parser.add_argument(
-            '--title', dest='title', required=True,
-            help='The title of the file to be used on archive.org. Will be both the title and identifier. Required.')
-        self.uploader.parser.add_argument(
-            '--description', dest='description', default='',
-            help='The description of the file to be used on archive.org. Optional. Default: empty')
-        self.uploader.parser.add_argument(
-            '--mediatype', dest='mediatype', default='web',
-            help='The media type of the file to be used on archive.org. Optional. Default: web')
-        self.uploader.parser.add_argument(
-            '--subject', dest='subject', default='wikiforge;wikiteam',
-            help='Subject (topics) of the file for archive.org. Multiple topics can be separated by a semicolon. Optional. Default: wikiforge;wikiteam')
-        self.uploader.parser.add_argument(
-            '--collection', dest='collection', default='opensource',
-            help='The name of the collection to use on archive.org. Optional. Default: opensource')
-        self.uploader.parser.add_argument(
-            '--file', dest='file', required=True,
-            help='The local path to the file to be uploaded to archive.org. Required.')
 
     def test_args(self):
         args = self.uploader.parser.parse_args(['--title', 'test_title', '--file', '/path/to/test_file'])
@@ -52,7 +32,7 @@ class TestArchiveUploader(unittest.TestCase):
         mtime = (now - timedelta(days=1)).timestamp()
         os.utime(f.name, (mtime, mtime))
 
-        self.uploader.parser.parse_args(['--title', 'test_title', '--file', f.name])
+        sys.argv[1:] = ['--title', 'test_title', '--file', f.name]
         self.uploader.upload()
 
         mock_item.upload.assert_called_once_with(f.name, metadata={
