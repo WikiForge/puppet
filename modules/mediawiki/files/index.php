920f027d92c86698ea28381ee16764f4968354ab
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-05-14 17:18:53 -0600

    Add an index.php entrypoint

---
+++ modules/mediawiki/files/index.php
@@ -0,0 +1,11 @@
+<?php
+require_once '/srv/mediawiki/config/initialise/WikiForgeFunctions.php';
+require WikiForgeFunctions::getMediaWiki( 'includes/WebStart.php' );
+if ( $wgArticlePath === '/wiki/$1' && $_SERVER['REQUEST_URI'] === '/' ) {
+	# Redirect to /wiki/ equivalent
+	$output = RequestContext::getMain()->getOutput();
+	$output->redirect( '/wiki/', 302 );
+	exit()
+}
+
+require WikiForgeFunctions::getMediaWiki( 'index.php' );
