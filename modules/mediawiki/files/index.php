bb074c2674ecc2c06c8d8abf463fe92153f29f27
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-05-14 17:32:38 -0600

    Full redirect

--- modules/mediawiki/files/index.php
+++ modules/mediawiki/files/index.php
@@ -1,9 +1,9 @@
 <?php
 require_once '/srv/mediawiki/config/initialise/WikiForgeFunctions.php';
 require WikiForgeFunctions::getMediaWiki( 'index.php' );
-if ( $wgArticlePath === '/wiki/$1' && $_SERVER['REQUEST_URI'] === '/' ) {
+if ( $wgArticlePath === '/wiki/$1' ) {
 	# Redirect to /wiki/ equivalent
 	$output = RequestContext::getMain()->getOutput();
-	$output->redirect( '/wiki/', 302 );
+	$output->redirect( '/wiki' . $_SERVER['REQUEST_URI'], 302 );
 	exit();
 }
