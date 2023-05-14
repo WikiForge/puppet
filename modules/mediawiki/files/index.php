9175c6b6047b90400eb17da9d8e8c3932492ba89
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-05-14 17:27:16 -0600

    Fix

--- modules/mediawiki/files/index.php
+++ modules/mediawiki/files/index.php
@@ -1,11 +1,9 @@
 <?php
 require_once '/srv/mediawiki/config/initialise/WikiForgeFunctions.php';
-require WikiForgeFunctions::getMediaWiki( 'includes/WebStart.php' );
+require WikiForgeFunctions::getMediaWiki( 'index.php' );
 if ( $wgArticlePath === '/wiki/$1' && $_SERVER['REQUEST_URI'] === '/' ) {
 	# Redirect to /wiki/ equivalent
 	$output = RequestContext::getMain()->getOutput();
 	$output->redirect( '/wiki/', 302 );
-	exit()
+	exit();
 }
-
-require WikiForgeFunctions::getMediaWiki( 'index.php' );
