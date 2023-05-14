def6ef70edf311c7d2705708b4f3ebdb854c06e8
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-05-14 17:38:49 -0600

    Delete index.php

--- modules/mediawiki/files/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '/srv/mediawiki/config/initialise/WikiForgeFunctions.php';
-require WikiForgeFunctions::getMediaWiki( 'index.php' );
-if ( $wgArticlePath === '/wiki/$1' ) {
-	# Redirect to /wiki/ equivalent
-	$output = RequestContext::getMain()->getOutput();
-	$output->redirect( '/wiki' . $_SERVER['REQUEST_URI'], 302 );
-	exit();
-}
