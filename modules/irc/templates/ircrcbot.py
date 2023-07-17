2c5ab6eb68561fcfb717f4faf1e70310cb37c3a7
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-07-16 22:39:44 -0600

    Try again

--- modules/irc/templates/ircrcbot.py
+++ modules/irc/templates/ircrcbot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import base64
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor, ssl
 from twisted.words.protocols import irc
@@ -32,51 +33,66 @@ class RCBot(irc.IRCClient):
 class SASLRCBot(RCBot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.sasl_mechanism = None
-        self.sasl_username = "<%= @sasl_username %>"
-        self.sasl_password = "<%= @sasl_password %>"
+        self.sasl_mechanism = "PLAIN"
+        self.sasl_username = "wikiforgebots"
+        self.sasl_password = "<%= @wikiforgebots_password %>"
+        self._sasl_in_progress = False
 
     def signedOn(self):
-        self.sendLine(b"CAP REQ :sasl")
+        self.sendLine("CAP REQ :sasl")
 
     def irc_CAP(self, prefix, params):
-        if params[1] == "ACK" and b"sasl" in params[2]:
+        if params[1] == "ACK" and "sasl" in params[2]:
             self.initiate_sasl()
 
     def initiate_sasl(self):
-        self.sendLine(b"AUTHENTICATE " + self.sasl_mechanism.encode("utf-8"))
+        self._sasl_in_progress = True
+        self.sendLine("AUTHENTICATE " + self.sasl_mechanism)
 
     def irc_900(self, prefix, params):
-        if params[1] == self.nickname and params[2] == b"SASL authentication successful":
+        if params[1] == self.nickname and params[2] == "SASL authentication successful":
             self.join(self.channel)
 
     def irc_904(self, prefix, params):
-        if params[1] == self.nickname and params[2] == b"SASL authentication failed":
+        if params[1] == self.nickname and params[2] == "SASL authentication failed":
             print("SASL authentication failed.")
             self.transport.loseConnection()
 
     def irc_906(self, prefix, params):
-        if params[1] == self.nickname and params[2] == b"SASL authentication aborted":
+        if params[1] == self.nickname and params[2] == "SASL authentication aborted":
             print("SASL authentication aborted.")
             self.transport.loseConnection()
 
     def irc_903(self, prefix, params):
-        if params[1] == self.nickname and params[2] == b"SASL authentication successful":
+        if params[1] == self.nickname and params[2] == "SASL authentication successful":
             self.join(self.channel)
 
     def sendLine(self, line):
-        super().sendLine(line)
+        if not self._sasl_in_progress:
+            super().sendLine(line.encode("utf-8"))
 
     def lineReceived(self, line):
-        if line == b"AUTHENTICATE +":
-            response = b"AUTHENTICATE " + self.sasl_mechanism.encode("utf-8") + b" " + self.encode_base64(self.sasl_username + "\x00" + self.sasl_username + "\x00" + self.sasl_password)
-            self.sendLine(response)
+        if line == "AUTHENTICATE +":
+            self.handle_sasl_challenge()
         else:
             super().lineReceived(line)
 
-    @staticmethod
-    def encode_base64(s):
-        return s.encode("utf-8").decode("base64")
+    def handle_sasl_challenge(self):
+        if self.sasl_mechanism == "PLAIN":
+            response = self.sasl_mechanism + " " + self.encode_plain()
+        else:
+            print("Unsupported SASL mechanism: %s" % self.sasl_mechanism)
+            self.transport.loseConnection()
+            return
+        self.sendLine(response)
+
+    def encode_plain(self):
+        return base64.b64encode(
+                self.encode("\x00%s\x00%s" % (self.sasl_username, self.sasl_password))
+            ).decode()
+
+    def encode_base64(self, s):
+        return s.encode("base64").strip()
 
 
 class RCFactory(protocol.ClientFactory):
