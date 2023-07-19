2a5ed01e45c95bcace9e7aff3ff4f25422f26909
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-07-19 15:49:21 -0600

    Facts

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -38,7 +38,7 @@ class nginx (
     }
 
     $module_path = get_module_path('varnish')
-    $cache_proxies = query_nodes("domain='${domain}' and Class['Role::Varnish']")
+    $cache_proxies = query_facts("domain='${domain}' and Class['Role::Varnish']")
     file { '/etc/nginx/nginx.conf':
         content => template('nginx/nginx.conf.erb'),
         require => Package['nginx'],
