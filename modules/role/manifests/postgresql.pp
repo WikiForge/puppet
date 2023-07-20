e966049e83c4e64995054f7c0db5213bbef31aad
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-07-20 15:10:00 -0600

    Remove for now

--- modules/role/manifests/postgresql.pp
+++ modules/role/manifests/postgresql.pp
@@ -6,22 +6,6 @@ class role::postgresql {
         use_ssl  => lookup('postgresql::ssl', {'default_value' => false}),
     }
 
-$query_results = query_facts('Class[Role::Puppetserver]', ['networking'])
-if $query_results {
-  $query_results.each |$key, $value| {
-    $ip = $value['networking']['ip']
-    $ip6 = $value['networking']['ip6']
-    if $ip and $ip6 {
-      ferm::service { "postgresql_${key}":
-        proto   => 'tcp',
-        port    => '5432',
-        srange  => "(${ip} ${ip6})",
-        notrack => true,
-      }
-    }
-  }
-}
-
     motd::role { 'role::postgresql':
         description => 'hosting postgresql server',
     }
