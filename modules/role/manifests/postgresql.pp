60b74a442277b7c2af13d80734483ecbaccc1d67
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-07-20 15:09:21 -0600

    .

--- modules/role/manifests/postgresql.pp
+++ modules/role/manifests/postgresql.pp
@@ -6,16 +6,13 @@ class role::postgresql {
         use_ssl  => lookup('postgresql::ssl', {'default_value' => false}),
     }
 
-$firewall_rules = {}
-
 $query_results = query_facts('Class[Role::Puppetserver]', ['networking'])
 if $query_results {
   $query_results.each |$key, $value| {
     $ip = $value['networking']['ip']
     $ip6 = $value['networking']['ip6']
     if $ip and $ip6 {
-      $resource_name = "postgresql_${key}"
-      $firewall_rules[$resource_name] = {
+      ferm::service { "postgresql_${key}":
         proto   => 'tcp',
         port    => '5432',
         srange  => "(${ip} ${ip6})",
@@ -24,8 +21,6 @@ if $query_results {
     }
   }
 }
-
-create_resources('ferm::service', $firewall_rules)
 
     motd::role { 'role::postgresql':
         description => 'hosting postgresql server',
