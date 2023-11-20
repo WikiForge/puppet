610a61d228f2304bdd4087e36357cb532826f12d
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-11-20 10:06:04 -0700

    Remove ircrcbot

--- modules/role/manifests/irc.pp
+++ modules/role/manifests/irc.pp
@@ -3,36 +3,12 @@ class role::irc {
     include irc::irclogbot
     include irc::relaybot
 
-    class { 'irc::ircrcbot':
-        nickname     => 'WikiForgeRC',
-        network      => 'irc.libera.chat',
-        network_port => '6697',
-        channel      => '#wikiforge-feed',
-        udp_port     => '5070',
-    }
-
     class { 'irc::irclogserverbot':
         nickname     => 'WikiForgeLSBot',
         network      => 'irc.libera.chat',
         network_port => '6697',
         channel      => '#wikiforge-sre',
         udp_port     => '5071',
-    }
-
-    $firewall_irc_rules_str = join(
-        query_facts('Class[Role::Mediawiki]', ['ipaddress', 'ipaddress6'])
-        .map |$key, $value| {
-            "${value['ipaddress']} ${value['ipaddress6']}"
-        }
-        .flatten()
-        .unique()
-        .sort(),
-        ' '
-    )
-    ferm::service { 'ircrcbot':
-        proto  => 'udp',
-        port   => '5070',
-        srange => "(${firewall_irc_rules_str})",
     }
 
     $firewall_all_rules_str = join(
