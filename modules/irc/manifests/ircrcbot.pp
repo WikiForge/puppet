0d5da93cc4fee172dd1df8cac4fa4de52cd753f5
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-11-20 09:59:21 -0700

    Absent ircrcbot

--- modules/irc/manifests/ircrcbot.pp
+++ modules/irc/manifests/ircrcbot.pp
@@ -11,15 +11,13 @@ class irc::ircrcbot(
     $wikiforgebots_password = lookup('passwords::irc::wikiforgebots')
 
     file { '/usr/local/bin/ircrcbot.py':
-        ensure  => present,
+        ensure  => absent,
         content => template('irc/ircrcbot.py'),
         mode    => '0755',
-        notify  => Service['ircrcbot'],
     }
 
     systemd::service { 'ircrcbot':
-        ensure  => present,
+        ensure  => absent,
         content => systemd_template('ircrcbot'),
-        restart => true,
     }
 }
