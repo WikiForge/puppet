7d26a9897749175872759ede0f32fc322bef8f76
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-12-14 20:04:29 -0700

    Add CSP

--- modules/mediawiki/manifests/nginx.pp
+++ modules/mediawiki/manifests/nginx.pp
@@ -5,6 +5,9 @@ class mediawiki::nginx {
     $sslcerts = loadyaml('/etc/puppetlabs/puppet/ssl-cert/certs.yaml')
     $php_fpm_sock = 'php/fpm-www.sock'
 
+    $module_path = get_module_path($module_name)
+    $csp = loadyaml("${module_path}/data/csp.yaml")
+
     nginx::conf { 'mediawiki-includes':
         ensure  => present,
         content => template('mediawiki/mediawiki-includes.conf.erb'),
