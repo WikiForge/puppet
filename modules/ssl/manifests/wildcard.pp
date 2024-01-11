3f02a4690af473757b449f829009f73710488fc4
Agent_Isai <43097272+AgentIsai@users.noreply.github.com>
2024-01-10 20:04:08 -0600

    Remove wikiforge.net wildcard

--- modules/ssl/manifests/wildcard.pp
+++ modules/ssl/manifests/wildcard.pp
@@ -21,26 +21,6 @@ define ssl::wildcard (
         $restart_nginx = undef
     }
 
-    if !defined(File["${ssl_cert_path}/wildcard.wikiforge.net.crt"]) {
-        file { "${ssl_cert_path}/wildcard.wikiforge.net.crt":
-            ensure => 'present',
-            source => 'puppet:///ssl/certificates/wildcard.wikiforge.net.crt',
-            notify => $restart_nginx,
-        }
-    }
-
-    if !defined(File["${ssl_cert_key_private_path}/wildcard.wikiforge.net.key"]) {
-        file { "${ssl_cert_key_private_path}/wildcard.wikiforge.net.key":
-            ensure    => 'present',
-            source    => 'puppet:///ssl-keys/wildcard.wikiforge.net.key',
-            owner     => 'root',
-            group     => $ssl_cert_key_private_group,
-            mode      => '0660',
-            show_diff => false,
-            notify    => $restart_nginx,
-        }
-    }
-
     if !defined(File["${ssl_cert_path}/inside.wf.crt"]) {
         file { "${ssl_cert_path}/inside.wf.crt":
             ensure => 'present',
