403b7721446ccf9eade0185de87ef0248b4b0ac1
Agent Isai <43097272+AgentIsai@users.noreply.github.com>
2024-02-12 13:17:53 -0600

    Update opensearch hostname

--- modules/opensearch/lib/puppet/provider/opensearch_rest.rb
+++ modules/opensearch/lib/puppet/provider/opensearch_rest.rb
@@ -85,7 +85,7 @@ class Puppet::Provider::OpensearchREST < Puppet::Provider
   #   an array of Hashes representing the found API objects, whether they be
   #   templates, pipelines, et cetera.
   def self.api_objects(protocol = 'https',
-                       host = 'os1.inside.wf',
+                       host = 'os21.inside.wf',
                        port = 9200,
                        timeout = 10,
                        username = nil,
