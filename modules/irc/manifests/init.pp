e5b3ff76c1741c0ddb944d4e3813c54cfd7c2cc8
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-02-15 11:07:28 -0700

    Use python3-irc again
    It has SASL support since https://github.com/jaraco/irc/commit/6bee9b0e2eef3eca9076a036c879d4e3c554dbee

--- modules/irc/manifests/init.pp
+++ modules/irc/manifests/init.pp
@@ -3,13 +3,8 @@ class irc {
     stdlib::ensure_packages([
         'python3',
         'python3-twisted',
+        'python3-irc',
         'python3-requests',
         'python3-requests-oauthlib',
     ])
-
-    exec { 'install_anisse_irc':
-        command => '/usr/bin/pip3 install git+https://github.com/anisse/irc',
-        creates => '/usr/local/lib/python3.11/dist-packages/irc',
-        require => Package['python3-pip'],
-    }
 }
