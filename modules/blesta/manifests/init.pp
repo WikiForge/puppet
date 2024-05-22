af425528614e6f4b99aa79e77b2e3e864415db4b
Agent Isai <43097272+AgentIsai@users.noreply.github.com>
2024-05-22 11:43:34 -0500

    blesta: load ioncube

--- modules/blesta/manifests/init.pp
+++ modules/blesta/manifests/init.pp
@@ -28,6 +28,7 @@ class blesta (
         'post_max_size' => '10M',
         'track_errors' => 'Off',
         'upload_max_filesize' => '10M',
+        'zend_extension' => '/usr/local/ioncube/ioncube_loader_lin_7.3.so',
     }
 
     $core_extensions =  [
